950caa63180dbed31e8932849072396750e8350a
FraRussotto <russottofrancesco1@gmail.com>
2023-09-18 16:40:10 +0200

    [ADD] JS playBtn

--- js/script.js
+++ js/script.js
@@ -1,28 +1,47 @@
 //1 Generare i quadrati che andranno ad inserirsi all'interno del container tramite un ciclo, richiamando innanzitutto il contenitore.
 
-const container = document.querySelector('.container_fr')
-
-//1.1 Creare e richiamare la funzione 'reset' per svuotare il container da elementi derivanti dall'html.
-reset();
+  //1.1 Creare e richiamare la funzione 'reset' per svuotare il container da elementi derivanti dall'html.
 
 //2 Creazione dei quadrati tramite il ciclo for che conterrà una funzione.
-
-//3 Inseriamo all'interno di una funzione separata, l'intero codice della costruzione dei quadrati.
-generateSquare(100)
-// for(let i = 1; i <= 100; i++){
-//   const square = addSquare(i);
-
+  
   //2.1 All'interno dello square, inseriamo anche EventListener per rendere attivo lo specifico quadrato nel momento del click da parte dell'utente attraverso l'utilizzo di 'this'.
-
-  // square.addEventListener('click', function(){
-    // this.classList.toggle('clicked');
-
+  
   //2.2 Tramite elemento.(qualsiasi testo) possiamo aggiungere un attributo custom, che di conseguenza possiamo far leggere attraverso 'console.log(this)'.
-    // square._indexSquare = (i)
-    // console.log(this._indexSquare)
-  // })
-
+  
   //2.3 Aggiungiamo uno square nel container ad ogni ciclo attraverso append.
+  
+//3 Inseriamo all'interno di una funzione separata, l'intero codice della costruzione dei quadrati.
+
+//4 Creare un bottone che avvi la generazione del container con i quadrati al suo interno.
+
+
+//1 
+const container = document.querySelector('.container_fr')
+const playBtn = document.querySelector('.play_btn')
+
+//2
+reset();
+
+//3
+
+//4
+const btn = document.createElement('button');
+btn.innerHTML = 'Play';
+btn.addEventListener('click', function(){
+  container.innerHTML = '';
+  generateSquare()
+})
+playBtn.append(btn);
+
+
+// for(let i = 1; i <= 100; i++){
+  // const square = addSquare(i);
+  // square.addEventListener('click', function(){
+  // this.classList.toggle('clicked');
+  // square._indexSquare = (i)
+  // console.log(this._indexSquare)
+  // });
+
   // container.append(square);
 // }
 
