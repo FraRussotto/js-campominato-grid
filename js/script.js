b7af3c7dcb636a15355c3f2c09626596a7c1f492
FraRussotto <russottofrancesco1@gmail.com>
2023-09-18 16:18:11 +0200

    [ADD] Function generateSquare

--- js/script.js
+++ js/script.js
@@ -7,22 +7,24 @@ reset();
 
 //2 Creazione dei quadrati tramite il ciclo for che conterrà una funzione.
 
-for(let i = 1; i <= 100; i++){
-  const square = addSquare(i);
+//3 Inseriamo all'interno di una funzione separata, l'intero codice della costruzione dei quadrati.
+generateSquare(100)
+// for(let i = 1; i <= 100; i++){
+//   const square = addSquare(i);
 
   //2.1 All'interno dello square, inseriamo anche EventListener per rendere attivo lo specifico quadrato nel momento del click da parte dell'utente attraverso l'utilizzo di 'this'.
 
-  square.addEventListener('click', function(){
-    this.classList.toggle('clicked');
+  // square.addEventListener('click', function(){
+    // this.classList.toggle('clicked');
 
   //2.2 Tramite elemento.(qualsiasi testo) possiamo aggiungere un attributo custom, che di conseguenza possiamo far leggere attraverso 'console.log(this)'.
-    square._indexSquare = (i)
-    console.log(this._indexSquare)
-  })
+    // square._indexSquare = (i)
+    // console.log(this._indexSquare)
+  // })
 
   //2.3 Aggiungiamo uno square nel container ad ogni ciclo attraverso append.
-  container.append(square);
-}
+  // container.append(square);
+// }
 
 // ------------ Function ------------ //
 
@@ -35,4 +37,18 @@ function addSquare(index){
   newSquare.className = 'square';
   newSquare.innerHTML = `<span>${index}</span>`;
   return newSquare;
+}
+
+//3 
+function generateSquare(numSquare){
+  for(let i = 1; i <= 100; i++){
+    const square = addSquare(i);
+
+    square.addEventListener('click', function(){
+    this.classList.toggle('clicked');
+    square._indexSquare = (i)
+    console.log(this._indexSquare)
+    })
+    container.append(square);
+  }
 }
